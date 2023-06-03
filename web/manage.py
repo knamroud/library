8323b9b067133a311aa66cc7ff54291a6f4e9059
Kirelos Namroud <namroudkirelos04@gmail.com>
2023-06-03 20:54:05 +0200

    bo roba che non funzionava ora funziona ma il progetto non funziona non è finito mi dispiace

--- web/manage.py
+++ web/manage.py
@@ -9,11 +9,14 @@ def main():
     # use different settings file for production, development, and testing
     if "DJANGO_SETTINGS_MODULE" not in os.environ:
         if len(sys.argv) > 1 and sys.argv[1] == "test":
-            os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library.settings.test")
-        elif len(sys.argv) > 1 and sys.argv[1] == "runserver":
-            os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library.settings.development")
+            os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                                  "library.settings.test")
+        elif os.environ.get("PRODUCTION", None):
+            os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                                  "library.settings.production")
         else:
-            os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library.settings.production")
+            os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                                  "library.settings.development")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -26,12 +29,12 @@ def main():
         from library_frontend.tsccthread import TSCCThread
         from django.conf import settings
         from os.path import join
-        #tscc = TSCCThread(join(settings.STATIC_ROOT, "ts"), join(
+        # tscc = TSCCThread(join(settings.STATIC_ROOT, "ts"), join(
         #    settings.STATIC_ROOT, "js"), debug=settings.DEBUG)
-        #tscc.start()
+        # tscc.start()
         execute_from_command_line(sys.argv)
     finally:
-        #tscc.stop()
+        # tscc.stop()
         pass
 
 
